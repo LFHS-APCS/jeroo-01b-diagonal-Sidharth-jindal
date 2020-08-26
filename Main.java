4d468eaccc002fc99bf50561c4f215fcbcc96d74
Sidharth-jindal <69948951+Sidharth-jindal@users.noreply.github.com>
2020-08-26 13:50:14 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main implements Directions {
      * Put your main Jeroo code here.
      */
     public static void runJerooCode() {
-Jeroo sid = new Jeroo(0,0EAST,10);
+Jeroo sid = new Jeroo(0,0,EAST,10);
 sid.plant();
 sid.turn(LEFT);
 sid.turn(LEFT);
